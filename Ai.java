997055993ba9082ee03ba09018782a4d5e4bd865
Aaron <aaron@ubuntu>
2014-01-21 16:17:10 -0500

    outlined ai, wrote vertical strategy

--- Ai.java
+++ Ai.java
@@ -3,45 +3,43 @@ import java.math.*;
 
 public class Ai {
     public int[][] board;
-    public boolean player;//true=1=red; false=2=black
+    public int player;//true=1=red; false=2=black
+    public int other;
 
     public Ai (int[][] in, boolean turn) {
-	board = in;
-	player = turn;
+        board = in;
+        if (turn) {
+	    player = 1;
+	    other = 2;
+	}
+	else {
+	    player = 2;
+	    other = 1;
+	}
     }
 
     public void test() {
-	for (int i=board.length-1;i>=0;i--) {
-	    for (int j=0;j<board[i].length;j++) {
-		System.out.print(board[i][j]);
-	    }
-	    System.out.println("");
-	}
+        for (int i=board.length-1;i>=0;i--) {
+            for (int j=0;j<board[i].length;j++) {
+                System.out.print(board[i][j]);
+            }
+            System.out.println("");
+        }
     }
 
     public int[][] dummy() {
-	boolean done = false;
-	double[] cols = new double [7];
-	int i = 0;
-	for (int j=0;j<cols.length;j++) {
-	    if (board[5][j] == 0)//ensures that the ai doesn't play in a full column, if possible.
-		cols[j] = Math.random();//replace this with sensical things
-	}
-	int x = 0;
-	for (int j=0;j<cols.length;j++)
-	    if (cols[j]>cols[x])
-		x = j;
-	System.out.println(Arrays.toString(cols));
-	while (i < 6 && !done){
-	    if (board[i][x] == 0){
-		if (player){
-		    board[i][x] = 1;
-		    done = true;
-		}
-		else{
-		    board[i][x] = 2;
-		    done = true;
-		}
+        boolean done = false;
+	double[] cols = ai1();
+        int i = 0;
+        int x = 0;
+        for (int j=0;j<cols.length;j++)
+            if (cols[j]>cols[x])
+                x = j;
+        System.out.println(Arrays.toString(cols));
+        while (i < 6 && !done){
+            if (board[i][x] == 0){
+		board[i][x] = player;
+		done = true;
 	    }
 	    else
 		i++;
@@ -52,6 +50,32 @@ public class Ai {
 	else {
 	    System.out.println("column full");
 	    return board;
-	}	
+	}        
+    }
+    
+    public double[] ai1() {
+	double[] cols = new double [7];
+	int getv,geth,getd1,getd2,blockv,blockh,blockd1,blockd2,noth,notd1,notd2,giveh,gived1,gived2;
+	for (int xcor=0;xcor<cols.length;xcor++) {
+	    int ycor = -1;
+	    for (int row=5;row>=0;row--) 
+		if (board[row][xcor] == 0)
+		    ycor=row;
+	    if (ycor != -1) {//ensures that the ai doesn't play in a full column, if possible
+		System.out.println("ycor"+ycor);
+		getv=0;blockv=0;
+		for (int i = ycor-1;i>ycor-4&&i>=0;i--) {//getv,blockv
+		    if (board[i][xcor] ==  player && board[i+1][xcor] != other && blockv==0)
+			getv++;
+		    if (board[i][xcor] ==  other && board[i+1][xcor] != player && getv==0)
+			blockv++;
+		}
+		getv++;
+		blockv++;
+		System.out.print("g/b:"+getv+blockv+". ");
+		cols[xcor] = getv+blockv;//replace this with sensical things
+	    }
+	}
+	return cols;
     }
 }
--- Gui.java
+++ Gui.java
@@ -16,169 +16,166 @@ public class Gui extends JFrame implements ActionListener {  //this code began a
     private JLabel singlePlayer,gameInfo;
 
     public void changeTurn() {
-	turn = !turn;
-	if (turn)
-	    this.gameInfo.setText("Red to Play");
-	else
-	    this.gameInfo.setText("Black to Play");
+        turn = !turn;
+        if (turn)
+            this.gameInfo.setText("Red to Play");
+        else
+            this.gameInfo.setText("Black to Play");
     }
     
     private class myKeyListener implements KeyListener {
-	public void keyPressed(KeyEvent e) {
-	    System.out.println("button");
-	    //fill in some box
-	}
-	
-	public void keyTyped(KeyEvent e) {
-	    System.out.println("potato");
-	}
-	public void keyReleased(KeyEvent e) {
-	    System.out.println("potato");
-	}
+        public void keyPressed(KeyEvent e) {
+            System.out.println("button");
+            //fill in some box
+        }
+        
+        public void keyTyped(KeyEvent e) {
+            System.out.println("potato");
+        }
+        public void keyReleased(KeyEvent e) {
+            System.out.println("potato");
+        }
     }
-	
-    public void actionPerformed(ActionEvent e) {	
-	if (e.getSource() == exitButton) {
-	    System.exit(0);
-	}
-	else if (e.getSource() == aiToggle) {
-	    useAi = !useAi;
-	    if (useAi) 
-		this.singlePlayer.setText("Singleplayer ON");
-	    else
-		this.singlePlayer.setText("Singleplayer OFF");
-	}
-	else{
-	    buttonAct(e.getSource(), ((JButton) e.getSource()).getText());
-	}
+        
+    public void actionPerformed(ActionEvent e) {        
+        if (e.getSource() == exitButton) {
+            System.exit(0);
+        }
+        else if (e.getSource() == aiToggle) {
+            useAi = !useAi;
+            if (useAi) 
+                this.singlePlayer.setText("Singleplayer ON");
+            else
+                this.singlePlayer.setText("Singleplayer OFF");
+        }
+        else{
+            buttonAct(e.getSource(), ((JButton) e.getSource()).getText());
+        }
     }
 
     public void updateBoard() {
-	boardBorder.removeAll();
-	for (int i=board.length-1;i>=0;i--) {
-	    for (int j=0;j<board[i].length;j++) {
-		JPanel jpanel = new JPanel();
-		jpanel.setBorder(BorderFactory.createLineBorder(Color.yellow,2));
-		if (board[i][j] == 1)
-		    jpanel.setBackground(Color.red);
-		else  if (board[i][j] == 2)
-		    jpanel.setBackground(Color.black);
-		else
-		    jpanel.setBackground(Color.blue);
-		boardBorder.add(jpanel);
-	    }
-	}
-	pane.revalidate();
+        boardBorder.removeAll();
+        for (int i=board.length-1;i>=0;i--) {
+            for (int j=0;j<board[i].length;j++) {
+                JPanel jpanel = new JPanel();
+                jpanel.setBorder(BorderFactory.createLineBorder(Color.yellow,2));
+                if (board[i][j] == 1)
+                    jpanel.setBackground(Color.red);
+                else  if (board[i][j] == 2)
+                    jpanel.setBackground(Color.black);
+                else
+                    jpanel.setBackground(Color.blue);
+                boardBorder.add(jpanel);
+            }
+        }
+        boardBorder.revalidate();
     }
     
     public void buttonAct(Object jb, String t){
-	int p;
-	if (turn)
-	    p = 1;
-	else
-	    p = 2;
-	System.out.println("player " + p + " picked column " + t);
-	int x = Integer.parseInt(t)-1;
-	int i = 0;
-	boolean done = false;
-	while (i < 6 && !done){
-	    if (board[i][x] == 0){
-		if (turn){
-		    board[i][x] = 1;
-		    done = true;
-		}
-		else{
-		    board[i][x] = 2;
-		    done = true;
-		}
-	    }
-	    else
-		i++;
-	}
-	if (done) {
-	    changeTurn();
-	    updateBoard();
-	    if (useAi) {
-		Ai a =new Ai(board,turn);
-		board =a.dummy();
-		changeTurn();
-		updateBoard();
-	    }
-	}
-	else
-	    System.out.println("Invalid move. Column full.");
+        int p;
+        if (turn)
+            p = 1;
+        else
+            p = 2;
+        System.out.println("player " + p + " picked column " + t);
+        int x = Integer.parseInt(t)-1;
+        int i = 0;
+        boolean done = false;
+        while (i < 6 && !done){
+            if (board[i][x] == 0){
+                if (turn){
+                    board[i][x] = 1;
+                    done = true;
+                }
+                else{
+                    board[i][x] = 2;
+                    done = true;
+                }
+            }
+            else
+                i++;
+        }
+        if (done) {
+            changeTurn();
+            updateBoard();
+            if (useAi) {
+                Ai a =new Ai(board,turn);
+                board =a.dummy();
+                changeTurn();
+                updateBoard();
+            }
+        }
+        else
+            System.out.println("Invalid move. Column full.");
     }
     
     public Gui() {
-	this.setTitle("My first GUI");
-	this.setSize(725,725);
-	this.setLocation(0,0);
-	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-	
-	pane = this.getContentPane();
-	pane.setLayout(new BorderLayout());
-	exitButton = new JButton("Exit");
-	aiToggle = new JButton("Toggle Singleplayer");
-	boardBorder = new JPanel();
-	boardBorder.setBorder(BorderFactory.createLineBorder(Color.yellow,5));
-	boardBorder.setLayout(new GridLayout(6,7));
-	board = new int[6][7];
-	b1= new JButton("1");
-	b2= new JButton("2");
-	b3= new JButton("3");
-	b4= new JButton("4");
-	b5= new JButton("5");
-	b6= new JButton("6");
-	b7= new JButton("7");
-	singlePlayer = new JLabel("Singleplayer OFF");
-	gameInfo = new JLabel("Red to Play");
-	buttons = new Container();
-	buttons.setLayout(new GridLayout(1,4));
-	buttons.add(exitButton);
-	buttons.add(gameInfo);
-	buttons.add(singlePlayer);
-	buttons.add(aiToggle);
-	buttonsBorder = new JPanel();
-	buttonsBorder.setBorder(BorderFactory.createLineBorder(Color.blue,5));
-	buttonsBorder.setLayout(new GridLayout(1,1));
-	buttons2 = new Container();
-	buttons2.setLayout(new GridLayout(1,7));
-	buttons2.add(b1);
-	buttons2.add(b2);
-	buttons2.add(b3);
-	buttons2.add(b4);
-	buttons2.add(b5);
-	buttons2.add(b6);
-	buttons2.add(b7);
-	buttonsBorder.add(buttons2);
-	pane.add(buttons,BorderLayout.PAGE_START);
-	pane.add(boardBorder,BorderLayout.CENTER);
-	pane.add(buttonsBorder,BorderLayout.PAGE_END);
-	for (int[] panels:board) {
-	    for (int panel:panels) {
-		JPanel jpanel = new JPanel();
-		jpanel.setBorder(BorderFactory.createLineBorder(Color.yellow,2));
-		jpanel.setBackground(Color.blue);
-		boardBorder.add(jpanel);
-	    }
-	}
-	
-	exitButton.addActionListener(this);
-	aiToggle.addActionListener(this);
-	b1.addActionListener(this);
-	b2.addActionListener(this);
-	b3.addActionListener(this);
-	b4.addActionListener(this);
-	b5.addActionListener(this);
-	b6.addActionListener(this);
-	b7.addActionListener(this);
+        this.setTitle("My first GUI");
+        this.setSize(725,725);
+        this.setLocation(0,0);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        
+        pane = this.getContentPane();
+        pane.setLayout(new BorderLayout());
+        exitButton = new JButton("Exit");
+        aiToggle = new JButton("Toggle Singleplayer");
+        boardBorder = new JPanel();
+        boardBorder.setBorder(BorderFactory.createLineBorder(Color.yellow,5));
+        boardBorder.setLayout(new GridLayout(6,7));
+        board = new int[6][7];
+        b1= new JButton("1");
+        b2= new JButton("2");
+        b3= new JButton("3");
+        b4= new JButton("4");
+        b5= new JButton("5");
+        b6= new JButton("6");
+        b7= new JButton("7");
+        singlePlayer = new JLabel("Singleplayer OFF");
+        gameInfo = new JLabel("Red to Play");
+        buttons = new Container();
+        buttons.setLayout(new GridLayout(1,4));
+        buttons.add(exitButton);
+        buttons.add(gameInfo);
+        buttons.add(singlePlayer);
+        buttons.add(aiToggle);
+        buttonsBorder = new JPanel();
+        buttonsBorder.setBorder(BorderFactory.createLineBorder(Color.blue,5));
+        buttonsBorder.setLayout(new GridLayout(1,1));
+        buttons2 = new Container();
+        buttons2.setLayout(new GridLayout(1,7));
+        buttons2.add(b1);
+        buttons2.add(b2);
+        buttons2.add(b3);
+        buttons2.add(b4);
+        buttons2.add(b5);
+        buttons2.add(b6);
+        buttons2.add(b7);
+        buttonsBorder.add(buttons2);
+        pane.add(buttons,BorderLayout.PAGE_START);
+        pane.add(boardBorder,BorderLayout.CENTER);
+        pane.add(buttonsBorder,BorderLayout.PAGE_END);
+        for (int[] panels:board) {
+            for (int panel:panels) {
+                JPanel jpanel = new JPanel();
+                jpanel.setBorder(BorderFactory.createLineBorder(Color.yellow,2));
+                jpanel.setBackground(Color.blue);
+                boardBorder.add(jpanel);
+            }
+        }
+        
+        exitButton.addActionListener(this);
+        aiToggle.addActionListener(this);
+        b1.addActionListener(this);
+        b2.addActionListener(this);
+        b3.addActionListener(this);
+        b4.addActionListener(this);
+        b5.addActionListener(this);
+        b6.addActionListener(this);
+        b7.addActionListener(this);
     }
-	
+        
     public static void main(String[] args) {
-	Gui g = new Gui();
-	g.setVisible(true);
+        Gui g = new Gui();
+        g.setVisible(true);
     }
-    
 }
-
-
